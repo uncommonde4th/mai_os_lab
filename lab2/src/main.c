a001f401b1dc2842d80ba463c45a165d43bc52e7
George Brezhnev <uncommonde4th@gmail.com>
2025-10-28 22:18:29 +0300

    lab2 v0.1

---
+++ lab2/src/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+typedef struct {
+    int array_count;
+    int array_size;
+    double **arrays;
+    double *result;
+    int start_index;
+    int end_index;
+} ThreadArgs;
+
+void *thread_func(void *arguments) {
+    ThreadArgs *args = (ThreadArgs*)arguments;
+
+    for (int i = args->start_index; i < args->end_index; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < args->array_count; j++) {
+            sum += args->arrays[j][i];
+        }
+        args->result[i] = sum;
+    }
+
+    return NULL;
+}
+
+void sequential_func(double **arrays, double *result, int array_count, int array_size) {
+    for (int i = 0; i < array_size; i++) {
+        result[i] = 0.0;
+        for (int j = 0; j < array_count; j++) {
+            result[i] += arrays[j][i];
+        }
+    }
+}
+
+
